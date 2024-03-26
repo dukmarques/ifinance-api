a3406d4733dd1b7e1ebb4335ad4b67da5c959be5
dukmarques <eduardomarques@outlook.com>
2024-03-25 23:35:14 -0300

    feat: add factories for user, card and category

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'id' => (string) Str::uuid(),
+            'name' => fake('pt_BR')->name(),
+            'email' => fake('pt_BR')->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => password_hash('Teste@123', PASSWORD_DEFAULT),
             'remember_token' => Str::random(10),
         ];
     }
