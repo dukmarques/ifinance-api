f735298ad75c41d16258a9fad4e18e1966c7b7de
dukmarques <eduardomarques@outlook.com>
2024-04-01 21:32:17 -0300

    feat: update api routes

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,14 @@
 
 use App\Http\Controllers\CardsController;
 use App\Http\Controllers\RevenuesController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\ExpensesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('revenues', RevenuesController::class);
     Route::apiResource('cards', CardsController::class);
+    Route::apiResource('categories', CategoriesController::class);
+    Route::apiResource('expenses', ExpensesController::class);
 });
