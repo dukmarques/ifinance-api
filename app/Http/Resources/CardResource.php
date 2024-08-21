7b39d9b49bd9608e862376ea9d6477576db8bd0b
dukmarques <eduardomarques@outlook.com>
2024-08-21 00:36:38 -0300

    feat(card): create CardResource

---
+++ app/Http/Resources/CardResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CardResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'closing_date' => $this->closing_date,
+            'due_date' => $this->due_date,
+            'user_id' => $this->user_id,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'user' => new UserResource($this->whenLoaded('user')),
+            'card_expenses' => CardExpensesResource::collection($this->whenLoaded('cardExpenses')),
+            'card_expenses_count' => $this->cardExpenses->count(),
+            'expenses' => ExpenseResource::collection($this->whenLoaded('expenses')),
+            'expenses_count' => $this->expenses->count(),
+        ];
+    }
+}
