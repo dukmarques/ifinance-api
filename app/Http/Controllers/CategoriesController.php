3e5768ade8d90294ae3aeec515cd23b29f17d14a
dukmarques <eduardomarques@outlook.com>
2024-10-01 22:59:37 -0300

    refactor(categories): extends BaseService and remove crud functions

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -2,101 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Categories\CreateCategoryRequest;
+use App\Http\Requests\Categories\UpdateCategoryRequest;
 use App\Services\CategoryService;
 use Illuminate\Http\Request;
 
-class CategoriesController extends Controller
+class CategoriesController extends BaseController
 {
-    private CategoryService $service;
-
     public function __construct(CategoryService $service) {
         $this->service = $service;
-    }
-
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(Request $request)
-    {
-        return $this->service->index();
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        try {
-            $this->validate($request, [
-                'name' => 'required|min:2'
-            ]);
-
-            $category = $this->service->store($request->all());
-            return response()->json($category, 201);
-        } catch (\Throwable $err) {
-            return response()->json([
-                'message' => $err->getMessage()
-            ], 400);
-        }
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        $category = $this->service->show($id);
-
-        if (!$category) {
-            return response()->json([
-                'message' => 'Category not found'
-            ], 404);
-        }
-
-        return response()->json($category);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        try {
-            $this->validate($request, [
-                'name' => 'filled|min:2'
-            ]);
-
-            $category = $this->service->update($id, $request->all());
-
-            if (!$category) {
-                return response()->json([
-                    'message' => 'Category not found'
-                ], 404);
-            }
-
-            return response()->json($category);
-        } catch (\Throwable $err) {
-            return response()->json([
-                'message' => $err->getMessage()
-            ], 400);
-        }
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        $category = $this->service->destroy($id);
-
-        if (!$category) {
-            return response()->json([
-                'message' => 'Category not found'
-            ], 404);
-        }
-
-        return response()->json([
-            'message' => 'Category deleted successfully'
-        ]);
+        $this->storeFormRequest = CreateCategoryRequest::class;
+        $this->updateFormRequest = UpdateCategoryRequest::class;
     }
 }
